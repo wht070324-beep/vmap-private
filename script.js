c0ba1b97f382af2a8a51348cfbd49d7c0850f0d5
WANG ht <wht070324@outlook.com>
2026-03-16 20:41:30 +0800

    换瓦片源为 Carto Voyager

--- script.js
+++ script.js
@@ -5,10 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
   var map = L.map('map').setView([22.543, 114.058], 11);  // 中心：深圳香港交界，缩放级别11
 
   // 添加 OpenStreetMap 瓦片层
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    maxZoom: 19
-  }).addTo(map);
+  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  subdomains: 'abcd',
+  maxZoom: 19
+}).addTo(map);
 
   // 加一个测试标记（未来替换成你的探险点）
   L.marker([22.543, 114.058])
